620464174f2360990eb95a796aab32eada32825c
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-06 13:36:15 -0700

    Remove `TasksIterableProvider` and related iterable registration logic.

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
-import {IterableService} from "@tokenring-ai/iterables";
 
 import * as chatCommands from "./chatCommands.ts";
 import packageJSON from './package.json' with {type: 'json'};
 import TaskService from "./TaskService.js";
-import TasksIterableProvider from "./TasksIterableProvider.js";
 import * as tools from "./tools.ts";
 
 export const packageInfo: TokenRingPackage = {
@@ -12,10 +10,6 @@ export const packageInfo: TokenRingPackage = {
   version: packageJSON.version,
   description: packageJSON.description,
   install(agentTeam: AgentTeam) {
-    agentTeam.services.waitForItemByType(IterableService).then((iterableService: IterableService) => {
-      iterableService.registerProvider("tasks", new TasksIterableProvider())
-    });
-
     agentTeam.addTools(packageInfo, tools)
     agentTeam.addChatCommands(chatCommands);
     agentTeam.addServices(new TaskService());
--- iterables.ts
+++
@@ -1,1 +0,0 @@
-export {default as tasks} from "./TasksIterableProvider.ts";
