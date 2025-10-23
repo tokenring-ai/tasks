75ca4ced1654f950060446500d9d7e6b99798c27
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:47 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -5,16 +5,16 @@ import packageJSON from './package.json' with {type: 'json'};
 import TaskService from "./TaskService.js";
 import * as tools from "./tools.ts";
 
-export const packageInfo: TokenRingPackage = {
+export default {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
   install(agentTeam: AgentTeam) {
-    agentTeam.addTools(packageInfo, tools)
+    agentTeam.addTools(packageJSON.name, tools)
     agentTeam.addChatCommands(chatCommands);
     agentTeam.addServices(new TaskService());
   },
-};
+} as TokenRingPackage;
 
 export {default as TaskService} from "./TaskService.ts";
 export type {Task} from "./state/taskState.ts";
