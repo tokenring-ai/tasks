10f1dd1bfae5bec3b52000e972d9ca7c11b35076
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:40 -0800

    Extract and modularize plugin logic into `plugin.ts`

--- index.ts
+++ index.ts
@@ -1,29 +1,2 @@
-import TokenRingApp from "@tokenring-ai/app"; 
-import {AgentCommandService} from "@tokenring-ai/agent";
-import {ChatService} from "@tokenring-ai/chat";
-import {TokenRingPlugin} from "@tokenring-ai/app";
-
-import chatCommands from "./chatCommands.ts";
-import contextHandlers from "./contextHandlers.ts";
-import packageJSON from './package.json' with {type: 'json'};
-import TaskService from "./TaskService.js";
-import tools from "./tools.ts";
-
-export default {
-  name: packageJSON.name,
-  version: packageJSON.version,
-  description: packageJSON.description,
-  install(app: TokenRingApp) {
-    app.waitForService(ChatService, chatService => {
-      chatService.addTools(packageJSON.name, tools);
-      chatService.registerContextHandlers(contextHandlers);
-    });
-    app.waitForService(AgentCommandService, agentCommandService =>
-      agentCommandService.addAgentCommands(chatCommands)
-    );
-    app.addServices(new TaskService());
-  },
-} as TokenRingPlugin;
-
 export {default as TaskService} from "./TaskService.ts";
 export type {Task} from "./state/taskState.ts";
