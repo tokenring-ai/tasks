702b433b81dc53672917665c810364faabfd1f6c
Mark Dierolf <mdierolf@financialcontent.com>
2026-07-24 16:34:22 -0700

    Switch plugin `config` to `configSchema`, refactor configuration handling

--- plugin.ts
+++ plugin.ts
@@ -32,5 +32,5 @@ export default {
       rpcService.registerEndpoint(tasksRPC);
     });
   },
-  config: packageConfigSchema,
+  configSchema: packageConfigSchema,
 } satisfies TokenRingPlugin<typeof packageConfigSchema>;
